b79a3c860c1e1343fd86c281568c2deaa6e5ded3
HongWei-luo <1501422801@qq.com>
2020-11-03 15:22:27 +0800

    Revert "同一返回格式"
    This reverts commit edc4e061

--- spring-cloud-dubbo-consumer/src/main/java/com/wei/shop/controller/DubboController.java
+++ spring-cloud-dubbo-consumer/src/main/java/com/wei/shop/controller/DubboController.java
@@ -2,10 +2,8 @@ package com.wei.shop.controller;
 
 
 import com.wei.shop.api.IDubboService;
-import com.wei.shop.conmom.Result;
+import com.wei.shop.domain.User;
 import org.apache.dubbo.config.annotation.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,10 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @date ：2020/8/11 9:57
  * @description：通过 `org.apache.dubbo` 包下的 `@Reference` 注解像调用本地服务一样调用远程服务
  */
-
 @RestController
 public class DubboController {
-    public final Logger log=LoggerFactory.getLogger(getClass());
+
     @Reference(version = "1.0.0")
     private IDubboService iDubboService;
 
@@ -29,11 +26,7 @@ public class DubboController {
     }
 
     @RequestMapping(value = "/getById/{id}",method = RequestMethod.GET)
-    public Result getById(@PathVariable("id") String id){
-        try {
-            return Result.ok(iDubboService.testMybatisPlus(id));
-        } catch (Exception e) {
-            return Result.error("请求失败");
-        }
+    public User getById(@PathVariable("id") String id){
+        return iDubboService.testMybatisPlus(id);
     }
 }
